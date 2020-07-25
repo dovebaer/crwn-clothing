9627dbfe439c35ec727014e9a258dee9887314ab
Bert Madwed <dovebaer@aol.com>
2020-07-24 23:46:58 -0400

    update our menu-component to add  withRouter as a higher order component HOC

--- React/crwn-clothing/src/App.js
+++ React/crwn-clothing/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>Hats</div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/Hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
